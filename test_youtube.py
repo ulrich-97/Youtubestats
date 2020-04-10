cb0214d15bb51b01777926f28c315192d1c8225e
Segen Tirfe <tirfesegen@gmail.com>
2020-04-09 22:45:57 -0400

    add unittest

--- test_youtube.py
+++ test_youtube.py
@@ -1,1 +1,13 @@
 import unittest 
+import docstrings
+
+class TestAccount(unittest.TestCase):
+    def test_init(self, edit):
+        pass
+    
+    def test_register(self, create_user, create_password, create_email,
+                 create_phonenum, login):
+        pass 
+    
+    def test_login(self, username_login, password_login):
+        pass
