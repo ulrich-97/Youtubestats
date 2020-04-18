3b8e25d20e7ba708e969a1b1fc05ae0cfa67982b
Oluwademilade Opaleke <themiladeyopaleke@gmail.com>
2020-04-18 16:21:41 -0400

    test changes
    made test

--- docstrings.py
+++ docstrings.py
@@ -7,7 +7,7 @@ class Account:
         create_user(str, int): users when creating accounts will create a username.
         create_password(str, int): users when creating accounts will create a password.
     """
-    
+    #this is a test comment
     def __init__(self, create_user, create_password):
         """Intializes the Account object"""
         
